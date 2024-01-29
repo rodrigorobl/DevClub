56a623a8e4cb7c822d53a3791da9ec3063148310
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-01-29 15:35:23 -0300

    atualizacao1

---
+++ JAVASCRIPT/Function.js
@@ -0,0 +1,19 @@
+/* Funcions / Funcoes
+        Trecho de codigo que executado
+        SOMENTE quando e solicitado ( Parente gente boa )
+*/
+
+const variavel = "abacatinho"
+
+console.log(variavel)
+
+function nomeNaTela() {
+    console.log("Rodolfo")
+}
+
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
