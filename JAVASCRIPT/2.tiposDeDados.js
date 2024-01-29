a3a5adcf70b257b7624dc6a17eb9afa89226b90b
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-01-29 15:01:31 -0300

    Atualizacao

--- JAVASCRIPT/2.tiposDeDados.js
+++ JAVASCRIPT/2.tiposDeDados.js
@@ -2,6 +2,57 @@
 Tipos de dados
 1. String -> Textos.
 2. Numbers -> Numeros
+3. Boolean -> Boleano (true and false)
+4. Object -> Objetos
+5. Null & Undefined
+
+Estrutura de Dados
+
+1.Array
+*/
+
+const users = [
+    {name: "Rodolfo", age: 30},
+    {name: "Aline", age: 27},
+]
+
+
+
+console.log(users[0].age)
+
+
+
+
+
+/*
+const users = [
+    {
+        Nome: "Rodolfo",
+        idade: 30,
+        nomeDoConjuge: "Aline",
+        id: null
+    },
+
+    {
+        Nome: "Aline",
+        idade: 27,
+        nomeDoConjuge: "Rodolfo",
+        id: null
+    },
+
+]
+
+console.log(users)
+*/
+
+
+
+
+
+
+
+/*
+------------------------------------------------------------------------
 
 const myAge = 30
 const myCar = 'Audi'
@@ -18,10 +69,41 @@ const texto3 = `textos com a crase` //Template literals ou Template String
 
 console.log(texto1)
 console.log(texto2)
-console.log(texto3)*/
+console.log(texto3)
 
 const number1 = 32 / 2
 const fakeNumber = "32 / 2"
 
 console.log(number1)
 console.log(fakeNumber)
+
+
+const rodolfo = {
+    name: "Rodolfo",
+    age: 30,
+    address: {
+        street: "Dos Bobos",
+        number: 0,
+        city: "Sao Paulo",
+        state: "SP",
+        country: "Brasil"
+    }
+
+}
+
+rodolfo.name = "Aline"
+
+console.log(rodolfo)
+
+
+
+const user = {
+    Nome: "Rodolfo",
+    idade: 30,
+    nomeDoConjuge: "Aline",
+    id: null
+}
+
+console.log(user.id)
+*/
+
