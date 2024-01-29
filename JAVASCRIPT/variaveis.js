13a3da93c71aaff201699a2ace793ad16b5c0751
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-01-29 13:38:36 -0300

    Alteracao javascript

---
+++ JAVASCRIPT/variaveis.js
@@ -0,0 +1,12 @@
+/*
+Variaveis
+
+    -let -> Pode alterar o valor quando quiser
+    -const -> Valor nao pode ser declarado novamente
+    -var -> Descontinuado / Nao usar
+
+*/
+
+let numeroDeAlunosDaSalaSete = 40
+
+console.log(numeroDeAlunosDaSalaSete)
