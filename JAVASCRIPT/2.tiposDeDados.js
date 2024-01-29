e73fcbd994afb03103ba7d464ff6a5b554f17e64
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-01-29 14:11:29 -0300

    ataulizacao

---
+++ JAVASCRIPT/2.tiposDeDados.js
@@ -0,0 +1,27 @@
+/*
+Tipos de dados
+1. String -> Textos.
+2. Numbers -> Numeros
+
+const myAge = 30
+const myCar = 'Audi'
+
+const myString = `Minha idade e ${myAge}, to ficando velho.
+Mas pelo manos to andando de ${myCar}`
+
+console.log(myString)
+
+
+const texto1 = "textos com aspas duplas"
+const texto2 = 'textos com aspas simples'
+const texto3 = `textos com a crase` //Template literals ou Template String
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)*/
+
+const number1 = 32 / 2
+const fakeNumber = "32 / 2"
+
+console.log(number1)
+console.log(fakeNumber)
