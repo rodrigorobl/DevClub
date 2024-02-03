4d341c45f802e646698cd5cf0266ebdc68631164
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-03 13:17:53 -0300

    mvp1

--- MVP/scripts.js
+++ MVP/scripts.js
@@ -2,9 +2,7 @@ function abrir() {
 
     document.querySelector("form").style.opacity = 1
     document.querySelector("div").style.opacity = 0.3
-
 }
-
 
 function fechar() {
     document.querySelector("form").style.opacity = 0
