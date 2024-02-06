01e0e491c7e86336087ca2343e23eb8dc5f5e833
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-06 16:58:40 -0300

    jav

---
+++ JAVASCRIPT/WEB/scripts.js
@@ -0,0 +1,19 @@
+/*const element = document.querySelector(".paragraph-js")
+
+
+element.textContent = "Texto novo <b>Ola</b>"
+
+element.innerHTML = ""
+    
+console.log(element.textContent) //so html
+console.log(element.innerText) //leva em conta o css
+console.log(element.innerHTML) // tras tudo -> Permite adicionar HTML*/
+
+
+
+const button = document.querySelector("h1")
+
+button.style.backgroundColor = "red"
+button.style.color = "#ffffff"
+button.style.fontSize = "100px"
+
