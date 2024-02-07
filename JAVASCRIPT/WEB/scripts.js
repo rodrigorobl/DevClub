9c6f342fedff122a80f4caad80eeae0a40a8e6ff
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-06 21:36:24 -0300

    js

--- JAVASCRIPT/WEB/scripts.js
+++ JAVASCRIPT/WEB/scripts.js
@@ -7,7 +7,7 @@ element.innerHTML = ""
     
 console.log(element.textContent) //so html
 console.log(element.innerText) //leva em conta o css
-console.log(element.innerHTML) // tras tudo -> Permite adicionar HTML*/
+console.log(element.innerHTML) // tras tudo -> Permite adicionar HTML
 
 
 
@@ -16,4 +16,15 @@ const button = document.querySelector("h1")
 button.style.backgroundColor = "red"
 button.style.color = "#ffffff"
 button.style.fontSize = "100px"
+*/
 
+const input = document.querySelector("#main-input")
+const paragrafo = document.querySelector(".novo-paragrafo")
+
+function cliqueiNoBotao(){
+    paragrafo.innerHTML = input.value
+}
+
+/*function digiteiNoInput(){
+    console.log("Digitei no input")
+}*/
