339596136ca5414d68016224743d4c3ed117931f
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-08 14:01:49 -0300

    sort

---
+++ JAVASCRIPT/Parte-3/1.Math.js
@@ -0,0 +1,14 @@
+/*
+
+Math -> Matematica
+- pow(2,2) / potencia / 2² = 4
+- sqrt(25,2) / raiz / √25
+- PI
+- ceil / teto -> arrendodar pra cima
+- floor / chao -> arredondar pra baixo
+
+- random() / numero aleatorio entre 0 e 1
+*/
+
+const result = Math.pow(2,3)
+    console.log(result)
