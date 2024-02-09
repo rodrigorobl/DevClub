34ecaf8f20e4974017c033e559408fed95e3d9c9
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-08 21:57:22 -0300

    aula5

--- JAVASCRIPT/Parte-3/7.Operador-ternario.js
+++ JAVASCRIPT/Parte-3/7.Operador-ternario.js
@@ -3,6 +3,19 @@ Operadores Ternario
 
 ? if(se)
 : else (se nao)
+&& if sem else
 
 */
+const salary = 10000
+/*if(salary < 3000){
+    console.log("Colaborador e Junior")
+}else if(salary >= 3000 && salary < 10000){
+    console.log("Colaborador e Senior")
+}else {
+    console.log("Colaborador e presidente")
+}*/
+// IF salary < 3000 && console.log("Colaborador e Junior")
 
+// IF/ELSE -> salary < 3000 ? console.log("Colaborador e Junior") : console.log("Colaborador e Senior")
+
+salary < 3000 ? console.log("Colaborador e Junior") : (salary >= 3000 && salary < 10000) ? console.log("Colaborador e Senior") : console.log("Colaborador e presidente")
---
+++ JAVASCRIPT/Parte-3/8.Switch Case.js
@@ -0,0 +1,25 @@
+/*
+
+    Switch Case - Controlador de Fluxo
+*/
+const temperatury = 20
+
+switch (temperatury) {
+        case 10:
+            console.log("Esta frio")
+        break
+        
+        case 20:
+            console.log("Esta meio frio")
+        break
+
+        case 30:
+            console.log("Esta quente")
+
+        break
+        
+        default:
+            console.log("Nao sei dizer")
+        break
+
+}
