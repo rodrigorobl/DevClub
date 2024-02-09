3c2861eebdf0c2ab7cfdd7096ee1d95588c791b8
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-09 14:26:53 -0300

    aula6

---
+++ JAVASCRIPT/Parte-4/1.For.js
@@ -0,0 +1,25 @@
+/*
+
+- FOR
+    1 Estrutura de repeticao - Criar uma variavel e colocar um valor inicial pra ela
+    2 Condicap - Enquanto for true, o laco continuara iterando.
+                Ele ira verificar antes de cada iteracao.
+
+    3 Expressao Final - O que ira ocorrer a cada vez que o nosso laco der uma volta
+
+    for ([inicializacao]; [condicao]; [expressao final]){
+        Seu codigo aqui
+    }
+
+
+*/
+
+const users = ["maria", "Joao", "Rodrigo", "Rodolfo", "maria", "Joao", "Rodrigo", "Rodolfo", "maria", "Joao", "Rodrigo", "Rodolfo", "maria", "Joao", "Rodrigo", "Rodolfo"]
+
+
+
+
+//users.lenght => Tamanho do meu array
+for (let i = 0; i < users.length; i++){
+    console.log(users[i])
+}
