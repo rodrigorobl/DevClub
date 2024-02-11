e96cbae1d64512e75716e0c102371817a99ccc13
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-11 18:02:15 -0300

    aula9

--- JAVASCRIPT/Parte-5/Functions.js
+++ JAVASCRIPT/Parte-5/Functions.js
@@ -3,8 +3,45 @@
     apenas quando e solicitado (Parente gente boa)
 */
 
+const myNumber = 0
 
-function sayMyName(name){
+
+
+
+function sub(value = myNumber, value2 = myNumber){
+    total1 = value - value2
+    console.log(`O valor sub e ${total1}.`)
+}
+
+function sum(value = myNumber, value2 = myNumber){
+    total2 = value + value
+    console.log(`O valor sun e ${total2}.`)
+    
+}
+
+function mult(value = myNumber, value2 = myNumber){
+    total3 = value * value
+    console.log(`O valor mult e ${total3}.`)
+    
+}
+
+function div(value = myNumber, value2 = myNumber){
+    total4 = value / value
+    console.log(`O valor div e ${total4}.`)
+    
+}
+sum(10, 30) 
+sub(30, 20)
+mult(10, 10)
+div(10,5)
+
+    console.log(`O valor total e ${total1+total2+total3+total4}.`)
+
+
+
+/*function sayMyName (name = "Rodrigo"){
     console.log(name)
 }
-sayMyName("Rodrigo")
+
+sayMyName("Luciene")
+*/
